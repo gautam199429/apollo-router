895c484606d9c24b452770009ed8cf5da2a9b252
Goutam Kumar Sah <gautam@vartulz.com>
2025-02-15 15:07:35 +0530

    ROUTER

--- subgraph/src/resolvers.js
+++ subgraph/src/resolvers.js
@@ -1,37 +1,32 @@
 const resolvers = {
   Query: {
-    accountSummary: () => {
-      // This would be replaced with logic to fetch account summary data
-      return {
-        accounts: [
-          {
-            accountReferenceId: "123456",
-            availableCreditAmount: {
-              availableSpendingCreditAmount: 5000.00,
-              availableCashCreditAmount: 1000.00
-            }
-          },
-          {
-            accountReferenceId: "789012",
-            availableCreditAmount: {
-              availableSpendingCreditAmount: 3000.00,
-              availableCashCreditAmount: 500.00
-            }
-          }
-        ]
-      };
-    }
+    accounts: () => [
+      {
+        accountReferenceId: "ACC001",
+        availableCreditAmount: {
+          availableSpendingCreditAmount: 2500.75,
+          availableCashCreditAmount: 1200.50,
+        },
+      },
+      {
+        accountReferenceId: "ACC002",
+        availableCreditAmount: {
+          availableSpendingCreditAmount: 3400.00,
+          availableCashCreditAmount: 1800.00,
+        },
+      },
+    ],
   },
 
   Account: {
-    // This can have more complex logic for each field if necessary
     accountReferenceId: (parent) => parent.accountReferenceId,
-    availableCreditAmount: (parent) => parent.availableCreditAmount
+    availableCreditAmount: (parent) => parent.availableCreditAmount,
   },
 
   AvailableCreditAmount: {
     availableSpendingCreditAmount: (parent) => parent.availableSpendingCreditAmount,
-    availableCashCreditAmount: (parent) => parent.availableCashCreditAmount
-  }
+    availableCashCreditAmount: (parent) => parent.availableCashCreditAmount,
+  },
 };
+
 export default resolvers;
