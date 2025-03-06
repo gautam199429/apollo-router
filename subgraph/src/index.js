2b203a5924cdbfb0cf7579ecf35c93e1aeb53397
Goutam Kumar Sah <gautam@vartulz.com>
2025-03-06 16:52:13 +0530

    HEADER

--- subgraph/src/index.js
+++ subgraph/src/index.js
@@ -8,7 +8,6 @@ import { mergeTypeDefs } from "@graphql-tools/merge";
 import customerresolvers from "./customerresolvers.js";
 import accountresolvers from "./accounsresolver.js";
 
-
 const resolvers = {
   Query: {
     ...accountresolvers.Query,
@@ -31,6 +30,26 @@ const server = new ApolloServer({
 
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4001 },
+  context: async ({ req }) => {
+    const entitlementsBypassHeader = req.headers["entitlements_bypass"];
+    console.log("ENTITLEMENTS_BYPASS Extracted:", entitlementsBypassHeader);
+    let entitlementsBypass = {};
+    if (entitlementsBypassHeader) {
+      try {
+        const cleanedHeader = entitlementsBypassHeader.startsWith("#")
+          ? entitlementsBypassHeader.slice(1)
+          : entitlementsBypassHeader;
+    
+        entitlementsBypass = JSON.parse(cleanedHeader);
+      } catch (error) {
+        console.error("Error parsing entitlements-bypass:", error);
+      }
+    }
+    console.log("Parsed ENTITLEMENTS_BYPASS:", entitlementsBypass);
+    for (const [key, value] of Object.entries(entitlementsBypass)) {
+      console.log(`KEY = ${key} , VALUE = ${value}`);
+    }
+    return { entitlementsBypass };
+  },
 });
-
-console.log(`🚀Server ready at: ${url}`);
+console.log(`🚀 Server ready at: ${url}`);
